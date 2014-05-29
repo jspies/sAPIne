78bab314f496368c96011c06b6ced358877f3a9d
Jonathan Spies <jonathan.spies@gmail.com>
2014-05-28 21:09:06 -0500

    sanity spec

--- spec/sapine_spec.rb
+++ spec/sapine_spec.rb
@@ -6,6 +6,6 @@ end
 
 describe "Sapine" do
   it "adds a method for common API index options" do
-    WithSapine.respond_to?(:index_options).should be(true)
+    WithSapine.new.respond_to?(:index_options).should be(true)
   end
 end
