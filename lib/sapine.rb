c3501483127bb1556006c9ad989bb9287463eec1
Jonathan Spies <jonathan.spies@gmail.com>
2014-05-28 21:08:36 -0500

    change to controller based

--- lib/sapine.rb
+++ lib/sapine.rb
@@ -2,19 +2,16 @@ require "sapine/version"
 
 module Sapine
   def self.included(base)
-    base.extend(ClassMethods)
+    
   end
-
-  module ClassMethods
-    def index_options(params)
-      chain = self.where(true)
-      chain = chain.where(state: params[:state]) if params[:state]
-      chain = chain.order(params[:order_by]) if params[:order_by]
-      if params[:limit].present?
-        chain = chain.limit(params[:limit])
-        chain = chain.offset(params[:page]) if params[:page].present?
-      end
-      chain
+  
+  def index_options(chain)
+    chain.where(state: params[:state]) if params[:state]
+    chain = chain.order(params[:order_by]) if params[:order_by]
+    if params[:limit].present?
+      chain = chain.limit(params[:limit])
+      chain = chain.offset(params[:page]) if params[:page].present?
     end
+    chain
   end
 end
--- spec/sapine_spec.rb
+++ spec/sapine_spec.rb
@@ -5,7 +5,7 @@ class WithSapine
 end
 
 describe "Sapine" do
-  it "adds a method like a scope" do
+  it "adds a method for common API index options" do
     WithSapine.respond_to?(:index_options).should be(true)
   end
 end
