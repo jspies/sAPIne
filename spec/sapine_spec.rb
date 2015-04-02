779cbb64d418cc43f3d1f166429dcfc0c3a0774d
Derek Victory <derekvictory@yahoo.com>
2015-04-02 17:24:33 -0500

    add elastic search support

--- spec/sapine_spec.rb
+++ spec/sapine_spec.rb
@@ -6,7 +6,7 @@ end
 
 describe "Sapine" do
   it "adds a method for common API index options" do
-    WithSapine.new.respond_to?(:index_options).should be(true)
+    expect(WithSapine.new.respond_to?(:index_options)).to be_truthy
   end
 
   context "index_options" do
@@ -17,37 +17,37 @@ describe "Sapine" do
     it "adds an order" do
       @ws.stub(:params).and_return({order_by: "state"})
       chain = @ws.index_options(TestModel)
-      chain.to_sql.include?("ORDER BY state").should be(true)
+      expect(chain.to_sql.include?("ORDER BY state")).to be_truthy
     end
 
     it "adds a negative order" do
       @ws.stub(:params).and_return({order_by: "-state"})
       chain = @ws.index_options(TestModel)
-      chain.to_sql.include?("ORDER BY state DESC").should be(true)
+      expect(chain.to_sql.include?("ORDER BY state DESC")).to be_truthy
     end
 
     it "adds the default limit" do
       @ws.stub(:params).and_return({})
       chain = @ws.index_options(TestModel)
-      chain.to_sql.should eql("SELECT  \"test_models\".* FROM \"test_models\"  LIMIT 20")
+      expect(chain.to_sql).to eql("SELECT  \"test_models\".* FROM \"test_models\" LIMIT 20 OFFSET 0")
     end
 
     it "adds a limit with a per_page" do
       @ws.stub(:params).and_return({per_page: 1})
       chain = @ws.index_options(TestModel)
-      chain.to_sql.should eql("SELECT  \"test_models\".* FROM \"test_models\"  LIMIT 1")
+      expect(chain.to_sql).to eql("SELECT  \"test_models\".* FROM \"test_models\" LIMIT 1 OFFSET 0")
     end
 
     it "adds an offset with a page param" do
       @ws.stub(:params).and_return({page: 2})
       chain = @ws.index_options(TestModel)
-      chain.to_sql.should eql("SELECT  \"test_models\".* FROM \"test_models\"  LIMIT 20 OFFSET 20")
+      expect(chain.to_sql).to eql("SELECT  \"test_models\".* FROM \"test_models\" LIMIT 20 OFFSET 20")
     end
 
     it "returns the count in api_meta" do
       @ws.stub(:params).and_return({})
       chain = @ws.index_options(TestModel)
-      @ws.api_meta.count.should be(4)
+      expect(@ws.api_meta[:count]).to eql(0)
     end
   end
 
@@ -58,13 +58,31 @@ describe "Sapine" do
     end
 
     it "returns the params per_page" do
-      @ws.api_meta[:per_page].should eql(12)
+      expect(@ws.api_meta[:per_page]).to eql(12)
     end
 
     it "calculates the pages" do
       TestModel.create
       @ws.index_options(TestModel)
-      @ws.api_meta[:pages].should eql(1)
+      expect(@ws.api_meta[:pages]).to eql(1)
+    end
+  end
+
+  context "elasticsearch" do
+    before do
+      @ws = WithSapine.new
+      @ws.stub(:params).and_return({page: 2, per_page: 2})
+    end
+
+    it "should return valid meta data" do
+
+      obj = double()
+      obj.stub(:total_entries).and_return(5)
+      @ws.elasticsearch_index_options(obj)
+
+      expect(@ws.api_meta[:count]).to eql(5)
+      expect(@ws.api_meta[:per_page]).to eql(2)
+      expect(@ws.offset).to eql(2)
     end
   end
 
